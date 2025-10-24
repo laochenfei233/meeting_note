cdbd55054de3b28e9cbef6b25340cf35dd482366
laochenfei233 <laochenfei233@example.com>
2025-10-24 12:56:05 +0800

    Fix WebRTC plugin registration for Linux platform

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+// Include flutter_webrtc plugin
+extern void FlutterWebRTCPluginRegisterWithRegistrar(FlPluginRegistrar* registrar);
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  FlutterWebRTCPluginRegisterWithRegistrar(
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterWebRTCPlugin"));
 }
